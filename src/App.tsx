fe3146db37e2925251c0e177cd4952882735ca34
Facu14carrizo <Facu14carrizo@gmail.com>
2026-03-12 16:37:02 -0300

    Fix assets

--- src/App.tsx
+++ src/App.tsx
@@ -63,8 +63,20 @@ export function App() {
       },
     ).addTo(map);
 
+    const markerIcon = L.icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      shadowSize: [41, 41],
+      shadowAnchor: [12, 41],
+      popupAnchor: [1, -34],
+    });
+
     const marker: Marker = L.marker([state.lat, state.lng], {
       draggable: true,
+      icon: markerIcon,
     }).addTo(map);
 
     function updateLocation(lat: number, lng: number) {
